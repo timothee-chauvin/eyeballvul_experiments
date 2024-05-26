840ef6619431c68bc2396d0782a9241cb9afd490
Timothee Chauvin <timothee.chauvin28@gmail.com>
2024-05-26 13:02:10 +0200

    merge both commit size plots into one

--- eyeballvul_experiments/eyeballvul_stats.py
+++ eyeballvul_experiments/eyeballvul_stats.py
@@ -13,20 +13,26 @@ def repo_size_histogram():
     sizes = sorted([cast(EyeballvulRevision, get_revision(commit)).size for commit in commits])
     df = pd.DataFrame({"sizes": sizes})
     df["cumulative_sum"] = df["sizes"].cumsum()
+
     sns.set_theme()
+    fig, ax = plt.subplots()
 
-    ax = sns.displot(
-        data=df["sizes"],
-        kde=True,
-        log_scale=True,
-        legend=False,
-        stat="density",
-    )
-    ax.set(
-        xlabel="size of the repository in bytes according to linguist", ylabel="number of commits"
-    )
-    plt.show()
+    sns.histplot(data=df["sizes"], kde=True, log_scale=True, ax=ax)
 
-    ax2 = sns.lineplot(data=df, x="sizes", y="cumulative_sum")
-    ax2.set_xscale("log")
+    ax2 = ax.twinx()
+
+    sns.lineplot(data=df, x="sizes", y="cumulative_sum", ax=ax2)
+
+    ax.set_xscale("log")
+
+    # Left y axis
+    ax.set_xlabel("size of the repository in bytes according to linguist")
+    ax.set_ylabel("number of commits")
+
+    # Right y axis
+    ax2.set_ylabel("cumulative sum of sizes")
+
+    # Remove the horizontal lines for the left y axis
+    ax.grid(which="both", axis="y", linestyle="")
+
     plt.show()
