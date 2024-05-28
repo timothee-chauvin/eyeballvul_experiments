449b956ece0135555c4fb446e3b22040a35d4741
Timothee Chauvin <timothee.chauvin28@gmail.com>
2024-05-28 12:47:24 +0200

    add fraction_of_benchmark_after_knowledge_cutoffs

--- eyeballvul_experiments/eyeballvul_stats.py
+++ eyeballvul_experiments/eyeballvul_stats.py
@@ -1,6 +1,7 @@
 """Various statistics on the eyeballvul benchmark."""
 
 import json
+from datetime import datetime
 from typing import cast
 
 import numpy as np
@@ -185,3 +186,56 @@ def plot_commits_and_vulns_by_date():
     )
 
     fig.write_image(Config.paths.plots / "commits_and_vulns_by_date.png", width=1920, height=1080)
+
+
+def fraction_of_benchmark_after_knowledge_cutoffs(dates: list[str]):
+    """
+    Save the following dictionary to Config.paths.results / "fraction_of_benchmark_after_knowledge_cutoffs.json":
+
+    {
+      date: {
+          "commits": fraction of the number of commits after the date,
+          "vulns": fraction of the number of vulnerabilities that were published after the date
+        }
+    }
+    for each date provided as input (as ISO 8601 strings)
+    """
+    commits = get_commits()
+    commit_dates = [get_revision(commit).date for commit in commits]
+    vulns = get_vulns()
+    vuln_dates = [vuln.published for vuln in vulns]
+    result = {}
+    for date_str in dates:
+        date = datetime.fromisoformat(date_str)
+        commits_after = sum(commit_date > date for commit_date in commit_dates)
+        vulns_after = sum(vuln_date > date for vuln_date in vuln_dates)
+        result[date_str] = {
+            "commits": commits_after / len(commits),
+            "vulns": vulns_after / len(vulns),
+        }
+    with open(
+        Config.paths.results / "fraction_of_benchmark_after_knowledge_cutoffs.json", "w"
+    ) as f:
+        json.dump(result, f, indent=2)
+        f.write("\n")
+
+
+if __name__ == "__main__":
+    plot_repo_size_histogram()
+    fraction_of_benchmark_covered_by_context_window(
+        [
+            128000,  # GPT-4o
+            200000,  # Claude 3
+            1000000,  # Gemini 1.5 Pro
+            10000000,  # Gemini 1.5 Pro in internal Google experiments
+        ]
+    )
+    plot_commits_and_vulns_by_date()
+    fraction_of_benchmark_after_knowledge_cutoffs(
+        [
+            "2023-09-01",  # Claude 3, "August 2023" as of 2024-05-28
+            "2023-11-01",  # GPT-4o, "October 2023" as of 2024-05-28
+            "2023-12-01",  # Gemini, "November 2023" as of 2024-05-28
+            "2024-01-01",  # gpt-4-turbo-2024-04-09, "December 2023" as of 2024-05-28
+        ]
+    )
