5f465e5e5ca16a09643c9d728364329896b4ecb4
Timothee Chauvin <timothee.chauvin28@gmail.com>
2024-06-09 11:20:08 +0200

    cmt

--- eyeballvul_experiments/score.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-
-class Score(BaseModel):
-    stats: dict[str, int] | dict[str, int | dict[str, int]]
-    mapping: dict[int, str]
-    date: datetime
